c56e63a89ee07480dc9f8877cf6bca5714c14d90
amir-mo1999 <amir.mohammad@hotmail.de>
2024-06-20 11:31:44 +0200

    added componenet for text input field

---
+++ frontend/src/components/TextInputField.tsx
@@ -0,0 +1,58 @@
+import { Dispatch, SetStateAction, useState } from "react"
+import { SxProps, Theme } from "@mui/system"
+import TextField from "@mui/material/TextField"
+import { InputBaseComponentProps } from "@mui/material"
+
+interface TextInputFieldProps {
+  valueSetter: Dispatch<SetStateAction<string>>
+  isError: boolean
+  setIsError: Dispatch<SetStateAction<boolean>>
+  label: string
+  inputProps?: InputBaseComponentProps
+  multiline?: boolean
+  minRows?: number
+  maxRows?: number
+  sx?: SxProps<Theme>
+}
+
+const TextInputField: React.FC<TextInputFieldProps> = ({
+  valueSetter,
+  isError,
+  setIsError,
+  label,
+  inputProps,
+  multiline,
+  minRows,
+  maxRows,
+  sx,
+}) => {
+  const [helperText, setHelperText] = useState<string>("")
+
+  return (
+    <TextField
+      sx={sx}
+      id="outlined-basic"
+      label={label}
+      variant="outlined"
+      required={true}
+      onChange={(e) => valueSetter(e.target.value)}
+      inputProps={inputProps}
+      helperText={helperText}
+      multiline={multiline ? multiline : false}
+      minRows={minRows ? minRows : undefined}
+      maxRows={maxRows ? maxRows : undefined}
+      error={isError}
+      onBlur={(e) => {
+        if (e.target.value === undefined) {
+          setIsError(true)
+          setHelperText("Please enter a function name")
+        } else {
+          setIsError(false)
+          setHelperText("")
+        }
+      }}
+    />
+  )
+}
+
+export { TextInputField }

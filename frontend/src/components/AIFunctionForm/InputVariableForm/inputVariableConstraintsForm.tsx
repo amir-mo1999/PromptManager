6b553c1a963a40653c585180f815a53088554abc
amir-mo1999 <amir.mohammad@hotmail.de>
2024-07-02 20:10:58 +0200

    cleanup

--- frontend/src/components/AIFunctionForm/InputVariableForm/inputVariableConstraintsForm.tsx
+++ frontend/src/components/AIFunctionForm/InputVariableForm/inputVariableConstraintsForm.tsx
@@ -4,7 +4,6 @@ import {
   NumericInputConstraintsObj,
   ImageFileInputConstraintsObj,
   AudioFileInputConstraintsObj,
-  inputVariableType,
   InputConstraints,
 } from "@/types"
 import { NumberInput } from "@/components"

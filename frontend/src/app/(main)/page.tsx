35280193ea49b5fe738ebf06008ba13521d17e9c
amir-mo1999 <amir.mohammad@hotmail.de>
2024-07-25 19:07:06 +0200

    some styling

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation"
 import { useAIFunctions } from "@/hooks"
 import { AIFunctionList } from "@/components"
 import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 
 export default function Home() {
   const router = useRouter()
@@ -26,6 +27,7 @@ export default function Home() {
           gap: "5px",
         }}
       >
+        <Typography variant="h5">AI Functions</Typography>
         <AIFunctionList sx={{ width: "30%" }} AIFunctions={AIFunctions}></AIFunctionList>
         <Button variant="contained" onClick={onClickCreateAIFunction}>
           Create AI Function
--- frontend/src/components/AIFunction/AIFunctionPaper.tsx
+++ frontend/src/components/AIFunction/AIFunctionPaper.tsx
@@ -2,14 +2,33 @@ import { AIFunctionT } from "@/types"
 import Paper from "@mui/material/Paper"
 import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
-
+import { alpha } from "@mui/material/styles"
+import { useState } from "react"
 interface AIFunctionPaperProps {
   AIFunction: AIFunctionT
 }
 
 const AIFunctionPaper: React.FC<AIFunctionPaperProps> = ({ AIFunction }) => {
+  const [isHover, setIsHover] = useState<boolean>(false)
+
+  function onMouseEnter() {
+    setIsHover(true)
+  }
+
+  function onMouseLeave() {
+    setIsHover(false)
+  }
+
   return (
-    <Paper sx={{ width: "100%" }}>
+    <Paper
+      elevation={isHover ? 15 : 2}
+      sx={{
+        width: "100%",
+        elevation: 15,
+      }}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <Typography variant="h6">{AIFunction.name}</Typography>
         <Typography variant="body1">{AIFunction.description}</Typography>
